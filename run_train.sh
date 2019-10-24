4eec526a3f0f47e1e763524900b01b8ef94d28e6
Pavelrst <rwcutout@gmail.com>
2019-10-24 12:29:44 -0700

    train script fixed

--- run_train.sh
+++ run_train.sh
@@ -1,3 +1,1 @@
-python main.py --dataset cifar10 --model resnet --depth 32 --batch-size 64 --device cuda --model-config "{'dropout' : 0.5 , 'dp_percentage' : 0.5 , 'dp_type' : 'weight' , 'device' : 'cuda'}"
-
-python main.py --dataset cifar10 --model resnet --depth 32 --batch-size 64 --device cuda --model-config "{'dropout' : 0 , 'dp_percentage' : 0 , 'dp_type' : 'weight' , 'device' : 'cuda'}"
+python main.py --dataset cifar10 --model resnet --batch-size 64 --device cuda --model-config "{'dropout' : 0 , 'dp_percentage' : 0 , 'dp_type' : 'weight' , 'device' : 'cuda'}"
