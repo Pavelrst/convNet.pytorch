6629a4dd535d3f50a7440f3f8aa16be9acbccda4
Pavelrst <rwcutout@gmail.com>
2019-10-26 06:25:01 -0700

    mm

--- run_train.sh
+++ run_train.sh
@@ -1,1 +1,1 @@
-python main.py --dataset cifar10 --model resnet --batch-size 64 --device cuda --model-config "{'dropout' : 0 , 'dp_percentage' : 0 , 'dp_type' : 'weight' , 'device' : 'cuda'}"
+python main.py
